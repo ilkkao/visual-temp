5861cf71d0278b6b7b83dc6148eadd5b8d0361c7
Ilkka Oksanen <iao@iki.fi>
2017-10-05 21:15:03 +0200

    Add first endpoints

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
 const PORT = 38000;
 
-app.use(async ctx => {
-  ctx.body = 'Visual temp';
+router.get('/', (ctx, next) => {
+  ctx.body = 'online';
 });
+
+router.get('/latest.json', (ctx, next) => {
+  ctx.body = {
+    ts: 0
+  };
+});
+
+router.post('/temperatures.json', (ctx, next) => {
+  ctx.body = {
+    result: 'ok'
+  };
+});
+
+router.get('/get')
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(PORT);
 
